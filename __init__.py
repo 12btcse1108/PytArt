3476578a13fd29afe77f194d127bc54b7942055c
12btcse1108 <nitinverma1394@gmail.com>
2018-04-28 13:15:00 +0530

    flask deployment on heroku

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+i#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr  3 01:04:49 2018
@@ -7,12 +7,27 @@ Created on Tue Apr  3 01:04:49 2018
 """
 
 from flask import Flask, render_template
+from da_mgmt import content
+
+content = content()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('main.html')
+    return render_template('dashboard.html', topic = content)
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/register/')
+def signout():
+    return render_template('register.html')
+
+@app.errorhandler(404)
+def page_not_found():
+    return "404 error something is not right"
 
 if __name__ == '__main__':
     app.run()
---
+++ da_mgmt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 23:22:07 2018
+
+@author: nitin
+"""
+
+def content():
+    content = {"article":[["python","/python"],
+                          ["java","/java"],
+                          ["javascript","/javascript"],
+                          ["pearl","/pearl"]
+                          ]}
+    return content
